4532c170d46bed1e2884d04d94633c2bfe67e6c1
Eri Fuerstenberg <ericfuerstenberg@gmail.com>
2020-04-26 14:30:45 -0700

    updating snapshot.py to list instance attributes, also introducing click module functionality

--- snapshot/snapshot.py
+++ snapshot/snapshot.py
@@ -1,8 +1,41 @@
 import boto3
+import click
+
+session = boto3.Session(profile_name='aws-snapshot')
+ec2 = session.resource('ec2')
+
+@click.command()
+def list_instances():
+	"List EC2 instances"
+	for i in ec2.instances.all():
+		print(', '.join((
+			i.id,
+			i.instance_type,
+			i.placement['AvailabilityZone'],
+			i.state['Name'],
+			i.public_dns_name)))
+	return
+
 
 if __name__ == '__main__':
-	session = boto3.Session(profile_name='aws-snapshot')
-	ec2 = session.resource('ec2')
-	
-	for i in ec2.instances.all():
-		print(i)
+	list_instances()
+
+
+
+
+# What features do we want?
+# What are the nouns and verbs our scripts will handle? 
+# Nouns: instances, volumes, snapshots
+# Verbs: take snapshot of instance, list instances, start and stop instances, add tags
+
+# snapshot list instances
+# snapshot instances list
+
+#snapshot snapshot instances
+#snapshot instances snapshot
+#snapshot instances create-snapshots
+#snapshot volumes list
+#snapshot instances stop tag=Project:project_tag
+#snapshot start instances --project=project_tag
+#snapshot instances stop
+
